0bc534f0111a6a1bc74dd578d66612c8946e41da
IgorGercc <igor.gerc@aquael.pl>
2026-07-22 11:58:13 +0200

    Use full Chromium for CI tests

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     baseURL: 'http://127.0.0.1:8765',
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
-    launchOptions: process.env.CI ? { args: ['--disable-gpu'] } : undefined,
+    launchOptions: process.env.CI ? { channel: 'chromium', args: ['--disable-gpu'] } : undefined,
   },
   projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
